0e908d8a0f21b66fcbcd5ca5cb83c2cc1416d3f5
Michael Brown <mbrown9792@gmail.com>
2015-07-11 09:27:43 -0500

    Second commit: I added the pairToTo, pairToFrom, pairToCC, pairCCFrom methods

--- parser.py
+++ parser.py
@@ -19,15 +19,51 @@ def count_email(n):
 	print("\nTOP %s EMAIL ADDRESSES FOUND!" %n)
 	print_out(Counter(email_list).most_common(n))
 
+def pairToTo():
+	pairs_list = []
+	for mess in mbox:
+		to = str(mess['to']).split(', ')
+		if (len(to) > 0):
+			for i in range(0, len(to)):
+				for j in range(i+1,len(to)):
+					print(str(to[i]) + "---" + str(to[j]))
+		else:
+			print("No results were found!")
+
+def pairToFrom():
+	pairs_list = []
+	for mess in mbox:
+		to = str(mess['to']).split(', ')
+		for i in range(0, len(to)):
+			print(str(to[i]) + "---" + str(mess['from']))
+
+def pairToCC():
+	pairs_list = []
+	for mess in mbox:
+		to = str(mess['to']).split(', ')
+		if (mess['cc'] != None):
+			cc = str(mess['cc']).split(', ')
+			for i in range(0, len(to)):
+				for j in range(0, len(cc)):
+					print(str(to[i]) + "---" + str(cc[j]))
+
+def pairCCFrom():
+	pairs_list = []
+	for mess in mbox:
+		if (mess['cc'] != None):
+			cc = str(mess['cc']).split(', ')
+			for i in range(0, len(cc)):
+				print(str(cc) + "---" + str(mess['from']))
+
 def print_out(a):
 	for email, count in a:
-   		print '---- %s  was found %d times' % (email, count)
+   		print '---- %s was found %d times' % (email, count)
 
 def main(filename):
     try:
     	global mbox
         mbox = mailbox.mbox(filename)
-        count_email(5)
+        pairToCC()
     except (IOError):
         print("cannot open " + filename)
         sys.exit(1)
